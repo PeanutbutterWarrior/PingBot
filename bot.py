b07a2115398d09b6bf90e1aebc35a1875b4e8f58
hellomeister3 <hellomeister3@gmail.com>
2021-10-26 21:29:16 +0100

    fix bonk, add dm

--- bot.py
+++ bot.py
@@ -25,13 +25,31 @@ async def on_ready():
         f'{client.user} is connected!'
     )
 
+def getMessage(msgctnt, cmdLen):
+    messageSplit = msgctnt.split(' ')
+    del messageSplit[0:cmdLen]
+    msg = ''
+    for word in messageSplit: msg += word + ' '
+    return msg[:-1]
+        
 @client.event
 async def on_message(message):
 
     msgctnt = message.content.lower()
 
-    ceres = await message.guild.query_members(user_ids=[625582561103446026])
-    ceres = ceres[0]
+    try:
+        ceres = await message.guild.query_members(user_ids=[625582561103446026])
+        ceres = ceres[0]
+            # destiny pings
+        if message.author.id == 621417848346247198:
+            await message.channel.send(f'oohh destiny tweet!! {ceres.mention}')
+
+        # mc pings
+        if message.author.id == 330657316196188172 and 'Java' in msgctnt:
+            await message.channel.send(f'oohh destiny tweet!! {ceres.mention}')
+    except:
+        pass
+            
 
     # cant reply to self
     if message.author == client.user:
@@ -41,42 +59,41 @@ async def on_message(message):
     if message.author.id == 899743143744925698:
         await message.channel.send(choice(craigSimp))
 
-    # destiny pings
-    if message.author.id == 621417848346247198:
-        await message.channel.send(f'oohh destiny tweet!! {ceres.mention}')
 
-    # mc pings
-    if message.author.id == 330657316196188172 and 'Java' in msgctnt:
-        await message.channel.send(f'oohh destiny tweet!! {ceres.mention}')
 
     # probs should do the command thingy for this but eh
     # general commands
     if '\\' == msgctnt[0]:
         if msgctnt[1:] == 'help':
             await message.channel.send('```help: this command\npingme: literally just pings you\nbonk [user] [times] [message]: pings [user] specified number of times, finishes with [message]```')
+        
         elif msgctnt[1:] == 'pingme':
             await message.channel.send(f'hi {message.author.mention}')
-        elif msgctnt[1:] == 'stop' and message.author == ceres:
+        
+        elif msgctnt[1:] == 'stop' and message.author.id == 625582561103446026:
             exit()
+        
         elif msgctnt[1:5] == 'bonk':
+            messageSplit = msgctnt.split(' ')
+            user = message.mentions[-1]
             try:
-                messageSplit = msgctnt.split(' ')
-                if len(messageSplit) < 3:
-                    raise IndexError
-                if len(message.mentions)>0:
-                    user = message.mentions[-1]
+                if len(messageSplit)>2:
+                        for i in range(int(messageSplit[2])):
+                            await message.channel.send(f'{user.mention} *bonk*')
+                            cmdLen = 3
                 else:
-                    raise IndexError
-                for i in range(int(messageSplit[2])):
-                    await message.channel.send(f'{user.mention} *bonk*')
-                bonkmsg = ''
-                if messageSplit[3]:
-                    for index in range(3, len(messageSplit)-1):
-                        bonkmsg += messageSplit[index] + ' '
-                    await message.channel.send(bonkmsg)
-            except IndexError as e:
-                await message.channel.send(f'`{msgctnt}` is an incomplete command. try `\help` for a list of commands')
-                print(e)
+                    raise ValueError
+            except ValueError:
+                await message.channel.send(f'{user.mention} *bonk*')
+                cmdLen = 2
+            bonkmsg = getMessage(msgctnt, cmdLen)
+            await message.channel.send(bonkmsg)
+
+        elif msgctnt[1:3] == 'dm':
+            user = message.mentions[-1]
+            await user.create_dm()
+            msg = getMessage(msgctnt, cmdLen=2)
+            await user.dm_channel.send(msg)
         else:
             await message.channel.send('hmmm, I\'m not sure I recognise that command. try `\help` for a list of commands')
         
